14e971a2de655b0d9b37a79a9c072bd26977e6ac
Valentin Squirelo <valentin+github@hackerloop.com>
2024-08-29 18:13:31 +0200

    remove useless test

--- playability.ino
+++ playability.ino
@@ -58,46 +58,23 @@ void loop() {
 
   if (!TinyUSBDevice.mounted()) return;
 
-  // Process BLE commands
   if (bleuart.available()) {
     uint8_t buf[64];
     int count = bleuart.read(buf, sizeof(buf));
 
     if (count > 0 && buf[0] == '!') {
       processCommand(buf, count);
-      // Send the updated gamepad report after processing the command
       usb_hid.sendReport(0, &gp, sizeof(gp));
     }
   }
-
-  // Call inputTest() function
-  inputTest();
-
-  // Add a delay to avoid running the test too frequently
-  delay(5000);  // Wait for 5 seconds before running the test again
 }
+
 void processCommand(uint8_t* buf, int count) {
   switch (buf[1]) {
     case 'A':
       if (count >= 4) {
         gp.x = buf[2];
         gp.y = buf[3];
-      }
-      break;
-    case 'B':
-      if (count >= 6) {
-        gp.buttons = (buf[2] << 24) | (buf[3] << 16) | (buf[4] << 8) | buf[5];
-      }
-      break;
-    case 'H':
-      if (count >= 3) {
-        gp.hat = buf[2];
-      }
-      break;
-    case 'T':
-      if (count >= 4) {
-        gp.rx = buf[2];
-        gp.ry = buf[3];
       }
       break;
     case 'Z':
@@ -112,34 +89,3 @@ void processCommand(uint8_t* buf, int count) {
   usb_hid.sendReport(0, &gp, sizeof(gp));
 }
 
-void inputTest() {
-  if (!usb_hid.ready()) return;
-
-  memset(&gp, 0, sizeof(gp));
-  usb_hid.sendReport(0, &gp, sizeof(gp));
-  delay(1000);
-
-  gp.x = 127;
-  gp.y = 0;
-  usb_hid.sendReport(0, &gp, sizeof(gp));
-  delay(1000);
-  gp.x = -127;
-  usb_hid.sendReport(0, &gp, sizeof(gp));
-  delay(1000);
-  gp.x = 0;
-  gp.y = -127;
-  usb_hid.sendReport(0, &gp, sizeof(gp));
-  delay(1000);
-  gp.y = 127;
-  usb_hid.sendReport(0, &gp, sizeof(gp));
-  delay(1000);
-
-  for (int i = 0; i < 16; i++) {
-    gp.buttons = (1U << i);
-    usb_hid.sendReport(0, &gp, sizeof(gp));
-    delay(500);
-  }
-
-  memset(&gp, 0, sizeof(gp));
-  usb_hid.sendReport(0, &gp, sizeof(gp));
-}
